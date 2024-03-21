97ac5ef2fffaaabb4e913754581116a5c112d71f
nikoaa <nikoaa@stud.ntnu.no>
2024-03-21 14:38:05 +0100

    Replaced Hashmap in class DeckOfCards with ArrayList

--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/DeckOfCards.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/DeckOfCards.java
@@ -1,35 +1,22 @@
 package no.ntnu.idatx2003.oblig3.cardgame;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Random;
 
 public class DeckOfCards {
-  private final HashMap<String, PlayingCard> deckOfCards;
-  private final String[] playingCardDeckReference;
+  private final ArrayList<PlayingCard> deckOfCards;
 
   public DeckOfCards() {
     char[] suits = {'H', 'D', 'C', 'S'};
-    this.playingCardDeckReference = new String[52];
-    this.deckOfCards = new HashMap<>();
+    this.deckOfCards = new ArrayList<>();
 
     int index = 0;
     for (char suit : suits) {
       for (int face = 1; face < 14; face++) {
         PlayingCard playingCard = new PlayingCard(suit, face);
-        this.deckOfCards.put(playingCard.getAsString(), playingCard);
-        this.playingCardDeckReference[index] = playingCard.getAsString();
+        this.deckOfCards.add(playingCard);
       }
     }
-  }
-
-  public ArrayList<PlayingCard> makeArrayListFromHashmap(HashMap<String, PlayingCard> hashMap) {
-    ArrayList<PlayingCard> arrayList= null;
-
-    if (hashMap != null) {
-      arrayList = new ArrayList<>(hashMap.values());
-    }
-    return arrayList;
   }
 
   public PlayingCardHand dealHand(int sizeOfHand) {
@@ -39,31 +26,21 @@ public class DeckOfCards {
       throw new IllegalArgumentException("More cards than what is in the deck cannot be dealt");
     }
 
-    ArrayList<PlayingCard> deckOfCardsList = makeArrayListFromHashmap(this.deckOfCards);
     PlayingCardHand playingCardHand = new PlayingCardHand();
     Random random = new Random();
     int count = 0;
 
     while (sizeOfHand > count) {
       int randomNumber = random.nextInt(deckOfCards.size());
-      PlayingCard playingCard = deckOfCardsList.get(randomNumber);
-      this.deckOfCards.remove(deckOfCardsList.get(randomNumber).getAsString());
+      PlayingCard playingCard = deckOfCards.get(randomNumber);
+      this.deckOfCards.remove(deckOfCards.get(randomNumber));
       playingCardHand.addPlayingCardToHand(playingCard);
       count++;
     }
     return playingCardHand;
   }
 
-  public void refillDeck() {
-    for (String k : this.playingCardDeckReference) {
-      if (!this.deckOfCards.containsKey(k)) {
-        char[] signAndFace = k.toCharArray();
-        this.deckOfCards.put(k, new PlayingCard(signAndFace[0], signAndFace[1]));
-      }
-    }
-  }
-
-  public HashMap<String, PlayingCard> getDeckOfCards() {
+  public ArrayList<PlayingCard> getDeckOfCards() {
     return this.deckOfCards;
   }
 }
